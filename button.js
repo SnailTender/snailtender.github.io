bc9bba0083ac01b69476a8aa7d41471e90285c84
aleitner <leitnersalex@gmail.com>
2020-04-20 18:57:14 -0400

    Lowered button

--- button.js
+++ button.js
@@ -1,17 +1,18 @@
 function Button(img, imgPressed) {
     this.img = img;
     this.imgPressed = imgPressed;
-    this.hb = new HitBox(canvasWidth / 50 * 26, canvasHeight / 10 * 7, canvasHeight / 15);
+    this.hb = new HitBox(canvasWidth / 50 * 26, canvasHeight * 34 / 50, canvasHeight / 15);
 
     this.draw = function () {
         if (this.hb.isPressed()) {
-            image(this.imgPressed, this.hb.x - this.imgPressed.width / 2, this.hb.y - this.imgPressed.height / 2);
+            image(this.imgPressed, this.hb.x - this.imgPressed.width / 2, this.hb.y - this.imgPressed.height *10/ 40);
         } else {
-            image(this.img, this.hb.x - this.img.width / 2, this.hb.y - this.img.height / 2);
+            image(this.img, this.hb.x - this.img.width / 2, this.hb.y - this.img.height *10/ 40);
         }
-    }
 
-    this.resize = function () {
-        this.hb = new HitBox(canvasWidth / 50 * 26, canvasHeight / 10 * 7, canvasHeight / 15);
+        // debug 
+        // fill(204, 101, 192, 127);
+        // stroke(127, 63, 120);
+        // ellipse(this.hb.x, this.hb.y, this.hb.r)
     }
 }
